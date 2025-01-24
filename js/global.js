97e1424a7404405bdcfa6b4d0b180f7632dd902d
Solat <solatpukhtunyar123@gmail.com>
2025-01-24 11:32:07 +0500

    Added ripple effect and theme effect.

--- js/global.js
+++ js/global.js
@@ -1,6 +1,30 @@
 /**
- * @copyright codewithsadee 2023
- * @author sadee <codewithsadee@gmail.com>
+ * @copyright solatpukhtunyar 2025
+ * @author solat <solatpukhtunyar123@gmail.com>
  */
 
 "use strict";
+
+/**
+ * Import
+ */
+
+import { ripple } from "./utils/ripple.js";
+
+/**
+ * Header On-Scroll State
+ */
+
+const /** {NodeElement} */ $header = document.querySelector("[data-header]");
+
+window.addEventListener("scroll", () => {
+  $header.classList[window.scrollY > 50 ? "add" : "remove"]("active");
+});
+
+/**
+ * Add ripple effect
+ */
+
+const /** {NodeList} */ $rippleElems =
+    document.querySelectorAll("[data-ripple]");
+$rippleElems.forEach(($rippleElem) => ripple($rippleElem));
--- js/utils/ripple.js
+++ js/utils/ripple.js
@@ -1,6 +1,51 @@
 /**
- * @copyright codewithsadee 2023
- * @author sadee <codewithsadee@gmail.com>
+ * @copyright solatpukhtunyar 2025
+ * @author solat <solatpukhtunyar123@gmail.com>
+ * @creditsTo sadee <codewithsadee@gmail.com>
  */
 
 "use strict";
+
+/**
+ * Adding Ripple Effect On An Element
+ * @param {Node} $rippleElem Element for Ripple Effect
+ */
+
+export const ripple = function ($rippleElem) {
+  $rippleElem.addEventListener("pointerdown", function (e) {
+    e.stopImmediatePropagation();
+
+    const /** {NodeElement} */ $ripple = document.createElement("div");
+    $ripple.classList.add("ripple");
+    this.appendChild($ripple);
+
+    const removeRipple = () => {
+      $ripple.animate(
+        {
+          opacity: 0,
+        },
+        { fill: "forwards", duration: 200 }
+      );
+      setTimeout(() => {
+        $ripple.remove();
+      }, 1000);
+    };
+    this.addEventListener("pointerup", removeRipple);
+    this.addEventListener("pointerleave", removeRipple);
+
+    const /** {Boolean} */ isNotIconButton =
+        !this.classList.contains("icon-btn");
+
+    if (isNotIconButton) {
+      const /** {Number} */ rippleSize = Math.max(
+          this.clientWidth,
+          this.clientHeight
+        );
+
+      $ripple.style.top = `${e.layerY}px`;
+      $ripple.style.left = `${e.layerX}px`;
+      $ripple.style.width = `${rippleSize}px`;
+      $ripple.style.height = `${rippleSize}px`;
+    }
+  });
+};
