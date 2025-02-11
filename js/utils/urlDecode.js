10b4d0819ab5a620ee39ad4e1e33ec7a6b2f1f56
Solat <solatpukhtunyar123@gmail.com>
2025-02-11 17:44:26 +0500

    Developed the rendering photos functionality.

--- js/utils/urlDecode.js
+++ js/utils/urlDecode.js
@@ -1,6 +1,23 @@
 /**
-* @copyright codewithsadee 2023
-* @author sadee <codewithsadee@gmail.com>
-*/
+ * @copyright solatpukhtunyar 2025
+ * @author solat <solatpukhtunyar123@gmail.com>
+ * @creditsTo sadee <codewithsadee@gmail.com>
+ */
 
 "use strict";
+
+/**
+ * Convert URL to Object
+ * @param {Object} urlString Url String
+ * @returns {Object} Url Object
+ */
+
+export const urlDecode = (urlString) => {
+  return Object.fromEntries(
+    urlString
+      .replace(/%23/g, "#")
+      .replace(/%20/g, " ")
+      .split("&")
+      .map((i) => i.split("="))
+  );
+};
